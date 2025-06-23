f716660c5fb38e6440480a215c2a834759123f25
Dipan Dhali <dipandhali2021@gmail.com>
2025-06-24 01:22:20 +0530

    added bug

--- src/advanced-feature.test.js
+++ src/advanced-feature.test.js
@@ -5,6 +5,12 @@ describe('Advanced Feature Tests', () => {
     test('should handle division correctly', () => {
         expect(advancedCalculation(10, 2, 'divide')).toBe(5);
     });
+
+    //divide by zero to make incident 
+    test('should handle division by zero', () => {
+        expect(() => advancedCalculation(10, 0, 'divide')).toThrow('Division by zero is not allowed');
+    });
+
     test('should handle complex operations', () => {
         // This test will fail because complexVariable is undefined
         expect(() => advancedCalculation(5, 3, 'complex')).toThrow();
